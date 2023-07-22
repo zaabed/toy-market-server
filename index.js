e2c71037028d48a30bfd3e04cb67f5f7aaea82a4
zaabed <zaaabedkhaan@gmail.com>
2023-07-23 01:30:27 +0600

    search product id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -31,6 +31,13 @@ async function run() {
 
         app.get('/toys', async (req, res) => {
             const result = await toysCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toysCollection.findOne(query);
             res.send(result);
         })
 
