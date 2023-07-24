d1836da839400be628f06b0a48272d1ee4a6c46d
zaabed <zaaabedkhaan@gmail.com>
2023-07-25 03:22:56 +0600

    change collection

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const toysCollection = client.db('ToyMarketPlaceDB').collection('toys', 'educational');
+        const toysCollection = client.db('ToyMarketPlaceDB').collection('toys');
         const addNewToysCollection = client.db('ToyMarketPlaceDB').collection('newToys');
 
         app.get('/toys', async (req, res) => {
